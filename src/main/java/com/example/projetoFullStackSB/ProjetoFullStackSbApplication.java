34401e53c715ab70fc4e56fd95aa6fe6eb69464e
Vitor Gabriel <128819057+VitorGabriel-TI@users.noreply.github.com>
2023-09-21 12:04:57 -0300

    commit inical
    Adição de arquivos criados no Spring Intializr.

---
+++ src/main/java/com/example/projetoFullStackSB/ProjetoFullStackSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoFullStackSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFullStackSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFullStackSbApplication.class, args);
+	}
+
+}
