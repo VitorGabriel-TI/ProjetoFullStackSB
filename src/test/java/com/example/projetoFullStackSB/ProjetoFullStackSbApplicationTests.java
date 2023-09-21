93cde4fde3798466ef1a0e361298d6cbc69f7912
Vitor Gabriel <128819057+VitorGabriel-TI@users.noreply.github.com>
2023-09-21 13:58:29 -0300

    commit inicial
    Adição de arquivos criados no Spring Intializr.

---
+++ src/test/java/com/example/projetoFullStackSB/ProjetoFullStackSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoFullStackSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFullStackSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
